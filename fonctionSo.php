e42712e273979e262a8ba8c3bb9c26439bf86f49
solene <solene@chezmoi.fr>
2017-11-13 14:37:29 +0100

    Suite

--- fonctionSo.php
+++ fonctionSo.php
@@ -4,9 +4,28 @@
 	{
 		$r=mysqli_connect('localhost','root','','commandes');
 		$t=mysqli_query($r,$query);
-		while($nuplet=mysqli_fetch_array($t))
-		{
-			print($nuplet[0]);
-		}
 		mysqli_close($r);
+		return ($t);
+	}
+	
+	function Printresults($tableau,$type)
+	{
+		if ($type = "checkbox")
+		{
+			while($nuplet=mysqli_fetch_array($t))
+			{
+				print("<input type=\"checkbox\" name=\"value[]\" value=\"".$nuplet[0]."\" id= b> ");
+				print("<label for= \"b\">".$nuplet[0]."\n</label><br><br>");
+			}
+		}
+		else
+		{
+			while($nuplet=mysqli_fetch_array($t))
+			{
+				print("<input type=\"radio\" name=\"val\" value=\"".$nuplet[0]."\" id= b> ");
+				print("<label for= \"b\">".$nuplet[0]."\n</label><br><br>");
+			}
+		}
+	}
+			
 ?>
